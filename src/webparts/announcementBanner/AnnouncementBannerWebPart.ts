08dbe3a657215fcdfe9840032cc4ce250e57873b
AYoho12 <143541100+AYoho12@users.noreply.github.com>
2024-07-01 18:35:03 +0000

    Add audience targeting

--- src/webparts/announcementBanner/AnnouncementBannerWebPart.ts
+++ src/webparts/announcementBanner/AnnouncementBannerWebPart.ts
@@ -25,6 +25,7 @@ export interface IAnnouncementBannerWebPartProps {
   selectedList: string;
   selectedItem: string;
   useListContent: boolean;
+  audienceTargeting: boolean;
 }
 
 const colors: Record<string, { lightcolor: string; darkcolor: string; fontcolor: string }> = {
@@ -65,6 +66,18 @@ export default class AnnouncementBannerWebPart extends BaseClientSideWebPart<IAn
     } else {
       title = this.properties.alertTitle;
       description = this.properties.alertDesc;
+    }
+
+    if(this.properties.audienceTargeting) {
+      const currentUserGroups = await sp.web.currentUser.groups.get();
+      const targetAudienceGroups = await sp.web.lists.getById(this.properties.selectedList).items.getById(parseInt(this.properties.selectedItem)).fieldValuesAsText.get();
+
+      const isInTargetAudience = currentUserGroups.some(group => targetAudienceGroups.includes(group.Title));
+
+      if (!isInTargetAudience) {
+        this.domElement.innerHTML = '';
+        return;
+      }
     }
   
     // Check if the alert banner should be displayed
@@ -125,6 +138,11 @@ protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
   const commonFields = [
     PropertyPaneToggle('useListContent', {
       label: 'Use Content from List',
+      onText: 'On',
+      offText: 'Off'
+    }),
+    PropertyPaneToggle('audienceTargetingEnabled', {
+      label: 'Enable Audience Targeting',
       onText: 'On',
       offText: 'Off'
     })
