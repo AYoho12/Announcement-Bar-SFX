c0b597e6e69aa1e1c2642ee79005378ecd003f67
Anthony Yoho <amy125@uakron.edu>
2024-07-08 17:08:10 -0400

    Fix warnings

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@
 const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
-
+build.addSuppression(`Warning - lint - src/webparts/announcementBanner/AnnouncementBannerWebPart.ts(102,45): error @typescript-eslint/no-explicit-any: Unexpected any. Specify a different type.`);
+build.addSuppression(`Warning - lint - src/webparts/announcementBanner/AnnouncementBannerWebPart.ts(111,78): error @typescript-eslint/no-explicit-any: Unexpected any. Specify a different type.`);
+build.addSuppression(`Warning - lint - src/webparts/announcementBanner/AnnouncementBannerWebPart.ts(111,93): error @typescript-eslint/no-explicit-any: Unexpected any. Specify a different type.`);
+build.addSuppression(`Warning - lint - src/webparts/announcementBanner/AnnouncementBannerWebPart.ts(165,80): error @typescript-eslint/no-explicit-any: Unexpected any. Specify a different type.`);
+build.addSuppression(`Warning - lint - src/webparts/announcementBanner/AnnouncementBannerWebPart.ts(168,17): error @typescript-eslint/no-explicit-any: Unexpected any. Specify a different type.`);
 var getTasks = build.rig.getTasks;
 build.rig.getTasks = function () {
   var result = getTasks.call(build.rig);
